ce3c1783db2150042db8b1295b2ae2b7f26a8dcc
eepyemy <emy.ict@proton.me>
2025-06-12 15:32:37 +0200

    fixed clean_a

--- .utils/clean_a.sh
+++ .utils/clean_a.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Deleting a.out files..."
-find $(git rev-parse --show-toplevel) -name "a.out" -name ".test" -delete -printf "removed %P\n"
+find $(git rev-parse --show-toplevel) -name "a.out" -delete -printf "removed %P\n" -or -name ".test" -delete -printf "removed %P\n"
